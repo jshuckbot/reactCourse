65259f1d080cac6bfd43c63e84bea6d0ae059135
Ivan <93859862+jshuckbot@users.noreply.github.com>
2024-08-04 00:37:30 -0400

    ready lesson 2 (#2)

---
+++ lesson_2/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TextInput from "./components/TextInput";
+import CommentsList from "./components/CommentsList";
+
+function App() {
+  return (
+    <div className="App">
+      <CommentsList />
+    </div>
+  );
+}
+
+export default App;
